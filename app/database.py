1f9171dfe8761a15dab4d987688e750ac0d101be
Marat22 <50494102+Marat22@users.noreply.github.com>
2025-05-09 14:16:21 +0500

    load credentials from .env (database.py)

--- app/database.py
+++ app/database.py
@@ -1,24 +1,18 @@
+import os
 from typing import Generator
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
+from dotenv import load_dotenv
 
-# Параметры подключения к БД
-username = ""
-password = ""  # Пароль из docker-compose.yml
-host = ""  # Имя сервиса БД в docker-compose.yml
-port = ""  # Внутренний порт PostgreSQL
-database = ""
-SQLALCHEMY_DATABASE_URL = f"postgresql://{username}:{password}@{host}/{database}"
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
-
-# Base = automap_base()
-# Base.prepare(autoload_with=engine) #point associate the DB engine with the Auto-map base.
 
 def get_db() -> Generator[Session, None, None]:
     db = SessionLocal()
